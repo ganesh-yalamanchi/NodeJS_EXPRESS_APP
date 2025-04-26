d7d48273bc51dc5ff07ae5ace9ebbd5ab246613e
ganesh-yalamanchi <ganeshyalamanchi128@gmail.com>
2025-04-26 18:35:56 +0530

    example_2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var example_1 = require('./src/example_1');
+var example_2 = require('./src/example_2');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello_world',example_1);
+app.use("/post_method",example_2);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
